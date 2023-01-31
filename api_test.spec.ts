f1b818280ab7f5fa39fb0a6694e2137cbf529d71
Yousaf Nabi <you@saf.dev>
2023-01-31 13:37:57 +0000

    feat: initial pact test for GET /pet/{petId}

--- api_test.spec.ts
+++ api_test.spec.ts
@@ -12,114 +12,153 @@
 
 import * as api from "./api"
 import { Configuration } from "./configuration"
+import { PactV3, MatchersV3 } from "@pact-foundation/pact";
 
 const config: Configuration = {}
 
+const provider = new PactV3({
+  consumer: "petstore-consumer",
+  provider: "petstore-provider",
+  logLevel: "trace",
+});
+
+const {
+  eachLike,
+  atLeastLike,
+  integer,
+  datetime,
+  boolean,
+  string,
+  regex,
+  like,
+  eachKeyLike,
+} = MatchersV3;
+
+
 describe("PetApi", () => {
   let instance: api.PetApi
-  beforeEach(function() {
-    instance = new api.PetApi(config)
-  });
+  // beforeEach(function() {
+  //   instance = new api.PetApi(config)
+  // });
 
-  test("addPet", () => {
-    const body: api.Pet = undefined
-    return expect(instance.addPet(body, {})).resolves.toBe(null)
-  })
-  test("deletePet", () => {
-    const petId: number = 789
-    const apiKey: string = "apiKey_example"
-    return expect(instance.deletePet(petId, apiKey, {})).resolves.toBe(null)
-  })
-  test("findPetsByStatus", () => {
-    const status: Array<string> = undefined
-    return expect(instance.findPetsByStatus(status, {})).resolves.toBe(null)
-  })
-  test("findPetsByTags", () => {
-    const tags: Array<string> = undefined
-    return expect(instance.findPetsByTags(tags, {})).resolves.toBe(null)
-  })
+  // test("addPet", () => {
+  //   const body: api.Pet = undefined
+  //   return expect(instance.addPet(body, {})).resolves.toBe(null)
+  // })
+  // test("deletePet", () => {
+  //   const petId: number = 789
+  //   const apiKey: string = "apiKey_example"
+  //   return expect(instance.deletePet(petId, apiKey, {})).resolves.toBe(null)
+  // })
+  // test("findPetsByStatus", () => {
+  //   const status: Array<string> = undefined
+  //   return expect(instance.findPetsByStatus(status, {})).resolves.toBe(null)
+  // })
+  // test("findPetsByTags", () => {
+  //   const tags: Array<string> = undefined
+  //   return expect(instance.findPetsByTags(tags, {})).resolves.toBe(null)
+  // })
   test("getPetById", () => {
     const petId: number = 789
-    return expect(instance.getPetById(petId, {})).resolves.toBe(null)
+
+    provider
+    .given(`A pet exists with id ${petId}`)
+    .uponReceiving(`A request for for pet with id ${petId}`)
+    .withRequest({
+      method: "GET",
+      path: `/pet/${petId}`,
+    })
+    .willRespondWith({
+      status: 200,
+      body: { name: like("doggie"), id:petId },
+    });
+
+    return provider.executeTest((mockserver) => {
+      instance = new api.PetApi(config, mockserver.url)
+      return instance.getPetById(petId, {}).then((result) => {
+         expect(result).toEqual({name: "doggie", id: petId});
+      });
+    });
+
   })
-  test("updatePet", () => {
-    const body: api.Pet = undefined
-    return expect(instance.updatePet(body, {})).resolves.toBe(null)
-  })
-  test("updatePetWithForm", () => {
-    const petId: number = 789
-    const name: string = "name_example"
-    const status: string = "status_example"
-    return expect(instance.updatePetWithForm(petId, name, status, {})).resolves.toBe(null)
-  })
-  test("uploadFile", () => {
-    const petId: number = 789
-    const body: Object = undefined
-    return expect(instance.uploadFile(petId, body, {})).resolves.toBe(null)
-  })
+  // test("updatePet", () => {
+  //   const body: api.Pet = undefined
+  //   return expect(instance.updatePet(body, {})).resolves.toBe(null)
+  // })
+  // test("updatePetWithForm", () => {
+  //   const petId: number = 789
+  //   const name: string = "name_example"
+  //   const status: string = "status_example"
+  //   return expect(instance.updatePetWithForm(petId, name, status, {})).resolves.toBe(null)
+  // })
+  // test("uploadFile", () => {
+  //   const petId: number = 789
+  //   const body: Object = undefined
+  //   return expect(instance.uploadFile(petId, body, {})).resolves.toBe(null)
+  // })
 })
 
-describe("StoreApi", () => {
-  let instance: api.StoreApi
-  beforeEach(function() {
-    instance = new api.StoreApi(config)
-  });
+// describe("StoreApi", () => {
+//   let instance: api.StoreApi
+//   beforeEach(function() {
+//     instance = new api.StoreApi(config)
+//   });
 
-  test("deleteOrder", () => {
-    const orderId: number = 789
-    return expect(instance.deleteOrder(orderId, {})).resolves.toBe(null)
-  })
-  test("getInventory", () => {
-    return expect(instance.getInventory({})).resolves.toBe(null)
-  })
-  test("getOrderById", () => {
-    const orderId: number = 789
-    return expect(instance.getOrderById(orderId, {})).resolves.toBe(null)
-  })
-  test("placeOrder", () => {
-    const body: api.Order = undefined
-    return expect(instance.placeOrder(body, {})).resolves.toBe(null)
-  })
-})
+//   test("deleteOrder", () => {
+//     const orderId: number = 789
+//     return expect(instance.deleteOrder(orderId, {})).resolves.toBe(null)
+//   })
+//   test("getInventory", () => {
+//     return expect(instance.getInventory({})).resolves.toBe(null)
+//   })
+//   test("getOrderById", () => {
+//     const orderId: number = 789
+//     return expect(instance.getOrderById(orderId, {})).resolves.toBe(null)
+//   })
+//   test("placeOrder", () => {
+//     const body: api.Order = undefined
+//     return expect(instance.placeOrder(body, {})).resolves.toBe(null)
+//   })
+// })
 
-describe("UserApi", () => {
-  let instance: api.UserApi
-  beforeEach(function() {
-    instance = new api.UserApi(config)
-  });
+// describe("UserApi", () => {
+//   let instance: api.UserApi
+//   beforeEach(function() {
+//     instance = new api.UserApi(config)
+//   });
 
-  test("createUser", () => {
-    const body: api.User = undefined
-    return expect(instance.createUser(body, {})).resolves.toBe(null)
-  })
-  test("createUsersWithArrayInput", () => {
-    const body: Array<api.User> = undefined
-    return expect(instance.createUsersWithArrayInput(body, {})).resolves.toBe(null)
-  })
-  test("createUsersWithListInput", () => {
-    const body: Array<api.User> = undefined
-    return expect(instance.createUsersWithListInput(body, {})).resolves.toBe(null)
-  })
-  test("deleteUser", () => {
-    const username: string = "username_example"
-    return expect(instance.deleteUser(username, {})).resolves.toBe(null)
-  })
-  test("getUserByName", () => {
-    const username: string = "username_example"
-    return expect(instance.getUserByName(username, {})).resolves.toBe(null)
-  })
-  test("loginUser", () => {
-    const username: string = "username_example"
-    const password: string = "password_example"
-    return expect(instance.loginUser(username, password, {})).resolves.toBe(null)
-  })
-  test("logoutUser", () => {
-    return expect(instance.logoutUser({})).resolves.toBe(null)
-  })
-  test("updateUser", () => {
-    const body: api.User = undefined
-    const username: string = "username_example"
-    return expect(instance.updateUser(body, username, {})).resolves.toBe(null)
-  })
-})
+//   test("createUser", () => {
+//     const body: api.User = undefined
+//     return expect(instance.createUser(body, {})).resolves.toBe(null)
+//   })
+//   test("createUsersWithArrayInput", () => {
+//     const body: Array<api.User> = undefined
+//     return expect(instance.createUsersWithArrayInput(body, {})).resolves.toBe(null)
+//   })
+//   test("createUsersWithListInput", () => {
+//     const body: Array<api.User> = undefined
+//     return expect(instance.createUsersWithListInput(body, {})).resolves.toBe(null)
+//   })
+//   test("deleteUser", () => {
+//     const username: string = "username_example"
+//     return expect(instance.deleteUser(username, {})).resolves.toBe(null)
+//   })
+//   test("getUserByName", () => {
+//     const username: string = "username_example"
+//     return expect(instance.getUserByName(username, {})).resolves.toBe(null)
+//   })
+//   test("loginUser", () => {
+//     const username: string = "username_example"
+//     const password: string = "password_example"
+//     return expect(instance.loginUser(username, password, {})).resolves.toBe(null)
+//   })
+//   test("logoutUser", () => {
+//     return expect(instance.logoutUser({})).resolves.toBe(null)
+//   })
+//   test("updateUser", () => {
+//     const body: api.User = undefined
+//     const username: string = "username_example"
+//     return expect(instance.updateUser(body, username, {})).resolves.toBe(null)
+//   })
+// })
 
